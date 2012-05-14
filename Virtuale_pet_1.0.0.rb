1befb4615d956753837365c58c11f581707cdb87
Rakhmanov Sergey <serga.mail@yandex.ru>
2012-05-14 15:08:58 +0400

    Change main file(4)

--- Virtuale_pet_1.0.0.rb
+++ Virtuale_pet_1.0.0.rb
@@ -1,4 +1,5 @@
-﻿help = true
+﻿code = true
+help = true
 eat = '10'
 energy = '10'
 air = '10'
@@ -9,43 +10,22 @@ puts 'Уважаемые пользователи, приложение рабо
 puts
 puts 'Это приложение, сделанное Рахмановым Сергеем, посторается симулировать Вашего '
 puts 'питомца. Кормите его, укладывайте спать, выводите гулять и он Вас полюбит.    '
-puts
-#Help
-if help == true
-  puts '================================== ПОДСКАЗКА =================================='
-  puts '1) Чтобы отключить подсказку напишите "0".'
-  puts '2) Первый показатель - еда, второй - энергия, 3 - желание гулять, 4 - любовь'
-  puts 'к Вам.'
-  puts '3) Чтобы посмотреть команды набирите "comands".'
-  puts '4) Для продолжения нпжмите "Enter".'
-  puts '==============================================================================='
+ansser_admin = gets.chomp
+if ansser_admin == 'admin'
+  puts 'Password:'
+  password = gets.chomp
+  if password == 'as1kl0vp'
+    code = false
+    help == false
+    system 'cls'
+	puts 'ok'
+  else
+    puts 'Error.'
+	exit
+  end
 end
-ansser = gets.chomp
-if ansser == '0'
-  help = false
-  puts '(Нажмите "Enter")'
-  ansser == gets.chomp
-end
-if ansser == 'comands'
-  puts
-  puts '1) to eat - дать поесть; 2) to sleep - уложить спать; 3) to walk - вывести гу-'
-  puts 'лять; 4) to respite - ничего не делать.'
-  puts '(Нажмите "Enter")'
-  ansser = gets.chomp
-end
-if ansser == ''
-  system 'cls'
-  puts 'Теперь начнём!'
-  puts
-  puts 'Вот настроение Вашего питомца:'
-  puts eat
-  puts energy
-  puts air
-  puts love
-  puts
-  puts 'Напишите команду. (если не знаете набирите "comands")'
-  puts
-  #Help
+#Help-----
+if code == true
   if help == true
     puts '================================== ПОДСКАЗКА =================================='
     puts '1) Чтобы отключить подсказку напишите "0".'
@@ -55,4 +35,57 @@ if ansser == ''
     puts '4) Для продолжения нпжмите "Enter".'
     puts '==============================================================================='
   end
+  ansser_for_help = gets.chomp
+  if ansser_for_help == '0'
+    help = false
+	code = false
+    puts 'Подсказка отключена. Нажмите "Enter".'
+  end
+  if ansser_for_help == 'comands'
+    puts
+    puts '1) to eat - дать поесть; 2) to sleep - уложить спать; 3) to walk - вывести гу-'
+    puts 'лять; 4) to respite - ничего не делать; и многие другие неважные команды (hello)'
+  end
+  if ansser_for_help == '5'
+    puts 'Вы хотите начать/продолжить? Хорошо! Нажмите "Enter".'
+  end
 end
+gets
+#---------  
+puts
+puts 'Вот настроение Вашего питомца:'
+puts eat
+puts energy
+puts air
+puts love
+puts
+puts 'Напишите команду. (если не знаете набирите "comands")'
+puts
+#Help-----
+if code == true
+  if help == true
+    puts '================================== ПОДСКАЗКА =================================='
+    puts '1) Чтобы отключить подсказку напишите "0".'
+    puts '2) Первый показатель - еда, второй - энергия, 3 - желание гулять, 4 - любовь'
+    puts 'к Вам.'
+    puts '3) Чтобы посмотреть команды набирите "comands".'
+    puts '4) Для продолжения нпжмите "Enter".'
+    puts '==============================================================================='
+  end
+  ansser_for_help = gets.chomp
+  if ansser_for_help == '0'
+    help = false
+	code = false
+    puts 'Подсказка отключена. Нажмите "Enter".'
+  end
+  if ansser_for_help == 'comands'
+    puts
+    puts '1) to eat - дать поесть; 2) to sleep - уложить спать; 3) to walk - вывести гу-'
+    puts 'лять; 4) to respite - ничего не делать; и многие другие неважные команды (hello)'
+  end
+  if ansser_for_help == '5'
+    puts 'Вы хотите начать/продолжить? Хорошо! Нажмите "Enter".'
+  end
+end
+gets
+#---------  
